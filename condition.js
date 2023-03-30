41b39905a5426d9163833bad8f87fbf6133eeab2
alamin <alamin.nava@gmail.com>
2023-03-30 17:58:55 +0600

    first commit

---
+++ condition.js
@@ -0,0 +1,42 @@
+// JavaScript Operators
+// Types of JavaScript Operators
+// There are different types of JavaScript operators:
+
+// Assignment Operators
+// Arithmetic Operators
+// Comparison Operators
+// String Operators
+// Logical Operators
+// Bitwise Operators
+// Ternary Operators
+// Type Operators
+
+
+//01. JavaScript Assignment Operators
+// For example
+let assignmentOperator = ()=>{
+    let x = 'MD AL AMIN';
+    let y = 24;
+    // console.log('Your Name is ' + x  + ' and your age is ' + y + ' years old')
+}
+assignmentOperator()
+
+// The Assignment Operator (=) assigns a value to a variable:
+
+//02. JavaScript Arithmetic Operators
+// For Example
+
+let arithmeticOperator = (a, b) =>{
+    // let a = x;
+    // let b = y;
+    
+
+    console.log('Result: '+ a + ' + ' + b + ' = '  + (a + b))
+    console.log(`Result: ${a} - ${b} = ${a - b}`)
+    console.log(`Result: ${a} * ${b} = ${a * b}`)
+    console.log(`Result: ${a} / ${b} = ${a / b}`)
+    console.log(`Result: ${a} % ${b} = ${a + b}`)
+
+}
+arithmeticOperator(20, 9)
+
